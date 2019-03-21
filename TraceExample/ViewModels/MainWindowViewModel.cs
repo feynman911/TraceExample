21b4acfad793c7505cdda78b2c0d3ce23074704f
feynman911 <feynman911@gmail.com>
2019-03-21 17:38:34 +0900

    プロジェクト ファイルを追加します。

---
+++ TraceExample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+using System.Diagnostics;
+
+namespace TraceExample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            string st = "";
+            for(int i = 0;i < 100; i++)
+            {
+                st = "テスト:" + i.ToString("D2");
+                Trace.WriteLine(st);
+            }
+            
+        }
+    }
+}
